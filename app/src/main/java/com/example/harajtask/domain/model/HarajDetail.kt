12da3ff38034d7b3c0638f7002f0d560b4db12d2
istiyawan@gmail.com <istiyawan@gmail.com>
2021-10-31 11:23:33 +0700

    modify car list item

--- app/src/main/java/com/example/harajtask/domain/model/HarajDetail.kt
+++ app/src/main/java/com/example/harajtask/domain/model/HarajDetail.kt
@@ -3,6 +3,7 @@ package com.example.harajtask.domain.model
 data class HarajDetail(
     val body: String,
     val city: String,
+
     val commentCount: Int,
     val date: String,
     val thumbURL: String,
